5b45eb36b4f25c790e5f7a77c5ee85ec7be5b1f7
hassen <hasansaid51@gmail.com>
2019-10-16 14:03:04 -0700

    index.ejs and data file

---
+++ models/cars.js
@@ -0,0 +1,24 @@
+const cars = [
+  {
+    name: "Honda",
+    color: "red",
+    gas: true
+  },
+  {
+    name: "Toyota",
+    color: "black",
+    gas: true
+  },
+  {
+    name: "Benz",
+    color: "blue",
+    gas: false
+  },
+  {
+    name: "Audi",
+    color: "purple",
+    gas: false
+  },
+]
+
+module.exports = cars
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cars = require("./models/cars.js")
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/cars', (req, res) => {
+  res.render("index.ejs", {
+    cars: cars
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
