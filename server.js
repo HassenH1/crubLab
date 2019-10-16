00807a7b73bd89ff40b2fa4963f7702161b71867
hassen <hasansaid51@gmail.com>
2019-10-16 14:43:39 -0700

    able to Delete obj

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cars = require("./models/cars.js")
+const methodOverride = require("method-override")
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
 
 app.get('/cars', (req, res) => {
   res.render("index.ejs", {
     cars: cars
   })
+})
+//add new cars---------not finished yet
+// app.get('/new', (req,res) => {
+//   res.render('new.ejs')
+// })
+//delete cars
+app.delete("/cars/:index", (req,res) => {
+  cars.splice(req.params.index,1)
+  res.redirect("/cars")
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
